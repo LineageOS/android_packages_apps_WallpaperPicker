b78740f7c23649ca53dde395719af04c1a8509b8
Mihai Preda <preda@android.com>
2011-06-11 13:31:55 +0200

    WallpaperPicker: add a "No Wallpaper" option
    This is a squash of the following commits.
    
    Author: Mihai Preda <preda@android.com>
    Date:   Sat Jun 11 13:31:55 2011 +0200
    
        Add a "No wallpaper" wallpaper option.
    
        The simplest implementation is with a black 1x1px image,
        but it could be implemented in a more efficient way in the future.
        (i.e. the black image is an implementation detail of the
        "no wallpaper" goal).
    
        On Nexus One (OLED) this "no wallpaper" produces
        45mA current saving compared to the "CM Earth" image wallpaper
        when display is set to max brightness, and 20mA saving on low brightness.
    
    Author: Keith Mok <kmok@cyngn.com>
    Date:   Fri May 27 09:49:38 2016 -0700
    
        Add a set wallpaper button for nowallpaper
    
        We set the wallpaper to black without a confirm
        button currently and without exit the wallpaper picker.
        This causes odd behaviour.
        Add back a confirm button like live wallpaper picker
    
        FEIJ-822
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Thu Oct 13 13:32:03 2016 -0700
    
        NoWallpaper: Clear lockscreen wallpaper too
    
    Author: Joey Rizzoli <joey@lineageos.org>
    Date:   Sat Jan 13 22:34:17 2018 +0200
    
        NoWallpaper: Add an adaptive icon
    
    Change-Id: Id712314d4e0808145f2180001bfc4dbeea766a46

---
+++ src/com/android/wallpaperpicker/NoWallpaper.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.wallpaperpicker;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.WallpaperManager;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import java.io.IOException;
+
+public class NoWallpaper extends Activity {
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+
+        setContentView(R.layout.wallpaper_preview);
+    }
+
+    public void setWallpaper(View v) {
+        WallpaperManager wm = WallpaperManager.getInstance(this);
+        new AlertDialog.Builder(this)
+            .setTitle(R.string.wallpaper_instructions)
+            .setItems(R.array.which_wallpaper_options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int selectedItemIndex) {
+                int whichWallpaper;
+                if (selectedItemIndex == 0) {
+                    whichWallpaper = WallpaperManager.FLAG_SYSTEM;
+                } else if (selectedItemIndex == 1) {
+                    whichWallpaper = WallpaperManager.FLAG_LOCK;
+                } else {
+                    whichWallpaper = WallpaperManager.FLAG_SYSTEM
+                            | WallpaperManager.FLAG_LOCK;
+                }
+
+                try {
+                    wm.setResource(R.drawable.black, whichWallpaper);
+                } catch (IOException e) {
+                }
+
+                setResult(RESULT_OK);
+                finish();
+            }
+        })
+        .show();
+    }
+}
