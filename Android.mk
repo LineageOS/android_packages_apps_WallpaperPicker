1678ca6cb8a502139b8d941868ba96d5f0c3f3f9
Joey <joey@lineageos.org>
2018-02-21 15:22:49 +0100

    WallpaperPicker: 15.1 wallpapers
    Source: https://unsplash.com
    
    Includes the following commits:
    
    Author: Michael Bestas <mkbestas@lineageos.org>
    Date:   Mon, 26 Feb 2018 23:58:50 +0200
    
        WallpaperPicker: Fix build for 1440p devices
    
        * Also move the 1080p wallpapers to a separate folder
    
        Change-Id: I6636e1406fb85eab983d5b78a7654bc05a3d7ba7
    
    Author: Dan Pasanen <dan.pasanen@gmail.com>
    Date:   Tue, 27 Feb 2018 07:43:17 -0600
    
        WallpaperPicker: use 1440p images when screen width is greater than 1080
    
        * Many device with odd resolutions, but still having a width
          greater than 1080, will benefit from these. This also fixes tablets
          which often have widths of 2560 who would want these higher res
          versions.
    
        Change-Id: I9a1ce7851634e515ae3f4afe78867f90fe7fb6af
    
    Change-Id: I9f4c579c012c3b732b6a8a0d0b6e6bcce4a9526e
    Signed-off-by: Joey <joey@lineageos.org>

--- Android.mk
+++ Android.mk
@@ -23,6 +23,15 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+ifeq ($(shell test $(TARGET_SCREEN_WIDTH) -gt 1080; echo $$?),0)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p
+else
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
+endif
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := WallpaperPicker
 include $(BUILD_PACKAGE)
